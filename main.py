4208b50a89732249343ab826907d66c1088bf565
Anton Romanov <ex2mple@yandex.ru>
2022-12-24 11:46:28 +0300

    v1.0

---
+++ main.py
@@ -0,0 +1,140 @@
+import os
+from os.path import join, dirname
+from dotenv import load_dotenv
+import discord
+import aiosqlite as sql
+
+
+load_dotenv(join(dirname(__file__), '.env'))
+TOKEN = os.environ.get("TOKEN")
+
+
+
+bot = discord.Bot(debug_guilds=[921377212500967444, 771736345437274132])
+
+
+@bot.event
+async def on_ready():
+    connection = await sql.connect('general.db')
+    await connection.execute('PRAGMA foreign_keys = ON;')
+    cursor = await connection.cursor()
+
+    try:
+
+        await cursor.execute('''CREATE TABLE if not exists guilds(
+                                 id INTEGER PRIMARY KEY AUTOINCREMENT,
+                                 name TEXT NOT NULL,
+                                 guild_id INTEGER UNIQUE NOT NULL
+                                 )''')
+
+        await cursor.execute('''CREATE TABLE if not exists users(
+                                 id INTEGER PRIMARY KEY AUTOINCREMENT,
+                                 name TEXT NOT NULL,
+                                 user_id INTEGER NOT NULL,
+                                 guild INTEGER NOT NULL,
+                                 cash INTEGER DEFAULT 300,
+                                 FOREIGN KEY (guild) REFERENCES guilds (guild_id) ON DELETE CASCADE
+                                 )''')
+
+        await cursor.execute('''CREATE TABLE if not exists currencies(
+                                 id INTEGER PRIMARY KEY AUTOINCREMENT,
+                                 guild INTEGER NOT NULL,
+                                 name TEXT NOT NULL,
+                                 price INTEGER DEFAULT 1,
+                                 max_price INTEGER,
+                                 FOREIGN KEY (guild) REFERENCES guilds (guild_id) ON DELETE CASCADE
+                                 )''')
+
+        await cursor.execute('''CREATE TABLE if not exists settings(
+                                 id INTEGER PRIMARY KEY AUTOINCREMENT,
+                                 guild INTEGER UNIQUE NOT NULL,
+                                 exchange INTEGER DEFAULT NULL,
+                                 exchange_info INTEGER DEFAULT NULL,
+                                 FOREIGN KEY (guild) REFERENCES guilds (guild_id) ON DELETE CASCADE
+                                 )''')
+
+        await cursor.execute('''CREATE TABLE if not exists wallets(
+                                 id INTEGER PRIMARY KEY AUTOINCREMENT,
+                                 user INTEGER NOT NULL,
+                                 guild INTEGER NOT NULL,
+                                 name TEXT NOT NULL,
+                                 amount INTEGER DEFAULT 0
+                                 )''')
+
+        await connection.commit()
+    except Exception as err:
+        print('Ошибка в работе кода: ', err)
+    finally:
+        await cursor.close()
+        await connection.close()
+
+    print(f'{bot.user} now is available')
+
+@bot.event
+async def on_guild_join(guild: discord.Guild):
+    connection = await sql.connect('general.db')
+    await connection.execute('PRAGMA foreign_keys = ON;')
+    cursor = await connection.cursor()
+
+    try:
+        await cursor.execute(f'''INSERT INTO guilds (name, guild_id) VALUES (?, ?)''', (guild.name, guild.id))
+        await connection.commit()
+    except Exception as err:
+        print('Ошибка в работе кода: ', err)
+    finally:
+        await cursor.close()
+        await connection.close()
+
+@bot.event
+async def on_guild_remove(guild: discord.Guild):
+    connection = await sql.connect('general.db')
+    await connection.execute('PRAGMA foreign_keys = ON;')
+    cursor = await connection.cursor()
+
+    try:
+        await cursor.execute('''DELETE FROM guilds WHERE guild_id=?''', (guild.id, ))
+        await connection.commit()
+    except Exception as err:
+        print('Ошибка в работе кода: ', err)
+    finally:
+        await cursor.close()
+        await connection.close()
+
+@bot.slash_command()
+async def test_guild_join(ctx):
+    if await bot.is_owner(ctx.author):
+        connection = await sql.connect('general.db')
+        await connection.execute('PRAGMA foreign_keys = ON;')
+        cursor = await connection.cursor()
+
+        try:
+            await cursor.execute(f'''INSERT INTO guilds (name, guild_id) VALUES (?, ?)''', (ctx.guild.name, ctx.guild.id))
+            await connection.commit()
+        except Exception as err:
+            print('Ошибка в работе кода: ', err)
+        finally:
+            await cursor.close()
+            await connection.close()
+
+@bot.slash_command()
+async def test_guild_remove(ctx):
+    if await bot.is_owner(ctx.author):
+        connection = await sql.connect('general.db')
+        await connection.execute('PRAGMA foreign_keys = ON;')
+        cursor = await connection.cursor()
+
+        try:
+            await cursor.execute('''DELETE FROM guilds WHERE guild_id=?''', (ctx.guild.id, ))
+            await connection.commit()
+        except Exception as err:
+            print('Ошибка в работе кода: ', err)
+        finally:
+            await cursor.close()
+            await connection.close()
+
+cogs_list = ['trading']
+for cog in cogs_list:
+    bot.load_extension(f'{cog}')
+
+if __name__ == "__main__":
+    bot.run(TOKEN)
---
+++ trading.py
@@ -0,0 +1,58 @@
+import discord
+import aiosqlite as sql
+import os
+from os.path import join, dirname
+from dotenv import load_dotenv
+
+load_dotenv(join(dirname(__file__), '.env'))
+
+
+class Trading(discord.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+        self.bank_logo = os.environ.get("bank_logo")
+    # exchange = discord.SlashCommandGroup('exchange', 'interact with exchange')
+
+    async def connect(self):
+        connection = await sql.connect('general.db')
+        await connection.execute('PRAGMA foreign_keys = ON;')
+        cursor = await connection.cursor()
+        return connection, cursor
+
+    @discord.slash_command(description='проверить баланс')
+    async def balance(self, ctx):
+        """Выведет пользователю баланс его кошелька."""
+        author, guild = ctx.author, ctx.guild
+        connection, cursor = self.connect(), self.connect()
+
+        try:
+            await cursor.execute(f'SELECT cash FROM users WHERE user_id = ? AND guild = ?',
+                                 (author.id, guild.id))
+            balance = await cursor.fetchone()
+
+            if not balance:
+                await cursor.execute(f'INSERT INTO users (name, user_id, guild) VALUES (?, ?, ?)',
+                                     (author.name, author.id, guild.id))
+                await connection.commit()
+                await self.balance(ctx)
+                return
+
+            embed = discord.Embed(
+                title='Ваш баланс:',
+                description=f'{balance[0]} <:cryptoruble:1055576687842181200>',
+                color=discord.Colour.gold()
+            )
+            embed.set_thumbnail(url=author.avatar)
+            embed.set_author(name='Банк', icon_url=self.bank_logo)
+            embed.set_footer(text=guild.name, icon_url=guild.icon)
+            await ctx.respond(embed=embed)
+        except Exception as err:
+            print('Ошибка в работе кода: ', err)
+        finally:
+            await cursor.close()
+            await connection.close()
+
+
+
+def setup(bot):
+    bot.add_cog(Trading(bot))
